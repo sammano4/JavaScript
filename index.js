ef733590d7ca725193ae92ca00d7f78afd01ae1b
Sam Mano <sammano044@gmail.com>
2026-06-07 16:57:13 +0530

    Added JavaScript files

---
+++ index.js
@@ -0,0 +1,70 @@
+// 04-06-2026 Java Script Task's
+
+// Question No - 1 Create 3 variables:
+let studentName = "Mano";
+let studentAge = 26;
+let studentCourse = "MERN Stack";
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);
+
+// Question No - 2 Var, Let, Const
+var company = "Stackly";
+let salary = 20000;
+const country = "India";
+company = "Google";
+salary = 30000;
+console.log(company);
+console.log(salary);
+console.log(country);
+
+// Question No - 3 User Name
+let userName = prompt("Enter Your Name:")
+console.log("Hello " + userName);
+
+// Question No - 4 Confirm Box
+let knowJS = confirm("Do you know Java Script");
+console.log(knowJS);
+
+// Question No - 5 Student Information
+let stuName = prompt("Enter Student Name:");
+let stuAge = prompt("Enter Student Age:");
+let stuCity = prompt("Enter Student City:");
+console.log("Name : " + stuName);
+console.log("Age : " + stuAge);
+console.log("City : " + stuCity);
+
+// Question No - 6 Alert Message
+let course = "JavaScript";
+alert("Welcome to " + course);
+console.log("Welcome to " + course);
+
+// Question No - 7 Document Write
+document.writeln("Hello Student <br>");
+document.writeln("Welcome to JavaScript <br><br>");
+console.log("Hello Student");
+console.log("Welcome to JavaScript");
+
+// Question No - 8 Favorite Things
+let favoriteFood = "Chicken Fry";
+let favoriteColor = "DarkBlack";
+let favoriteMovie = "Oppenheimer ";
+console.log("Food : " + favoriteFood);
+console.log("Color : " + favoriteColor);
+console.log("Movie : " + favoriteMovie);
+
+// Question No - 9 Mini Bio Data
+let name = "Mano";
+let age = 26;
+let phone = "9087504181";
+let email = "sammano044@gmail.com";
+console.log("Name : " + name);
+console.log("Age : " + age);
+console.log("Phone : " + phone);
+console.log("Email : " + email);
+
+// Question No - 10 Challenge
+let userName2 = prompt("Enter Your Name:");
+let userCourse = prompt("Enter Your Course:");
+alert("Hello " + userName2 + ", Welcome to " + userCourse + " Course");
+console.log("Hello " + userName2 + ", Welcome to " + userCourse + " Course");
